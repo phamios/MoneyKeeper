f258f74a51257785fecd8e58e687e5c228e82ebb
Son Pham <sonpxvn@gmail.com>
2017-02-10 02:10:35 +0700

    create model file

---
+++ app/src/main/java/com/bot/redsun/moneykeeper/Adapter/HistoryListAdapter.java
@@ -0,0 +1,8 @@
+package com.bot.redsun.moneykeeper.Adapter;
+
+/**
+ * Created by redsu on 10/02/2017.
+ */
+
+public class HistoryListAdapter {
+}
---
+++ app/src/main/java/com/bot/redsun/moneykeeper/Model/UserGroupModel.java
@@ -0,0 +1,8 @@
+package com.bot.redsun.moneykeeper.Model;
+
+/**
+ * Created by redsu on 10/02/2017.
+ */
+
+public class UserGroupModel {
+}
